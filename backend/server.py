be3d4094da812f38e1e8df538c9583251ad99275
Lisa Anne Hildreth <lisaannehildreth@gmail.com>
2026-02-20 16:34:13 -0600

    Fix CORS for routecastweather.com

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, APIRouter, HTTPException
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from starlette.middleware.cors import CORSMiddleware
 from motor.motor_asyncio import AsyncIOMotorClient
@@ -275,6 +276,16 @@ NOAA_HEADERS = {
 
 # Create the main app
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://routecastweather.com",
+        "https://www.routecastweather.com"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Create routers
 api_router = APIRouter(prefix="/api")
